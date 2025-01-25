14c27d0d307fac979c1a75e5ad6072a1df14f6d6
khaled yassen <79770764+khaledyassen@users.noreply.github.com>
2025-01-25 17:40:48 +0200

    Add files via upload

---
+++ Auot_JS_Analysis/requirements.sh
@@ -0,0 +1,9 @@
+# install Linkfinder
+git clone https://github.com/GerbenJavado/LinkFinder.git
+pip3 install -r LinkFinder/requirements.txt
+python3 LinkFinder/setup.py install
+
+# install SecretFinder
+git clone https://github.com/m4ll0k/SecretFinder.git
+pip3 install -r SecretFinder/requirements.txt
+pip3 install termcolor
